b166aa3dda5635e25300cafd2bd2adb3b9074f43
Nassim Sakhri <tails01@hotmail.fr>
2024-12-04 13:53:31 +0100

    ajout singleton

---
+++ exoSingleton/src/ConfigurationManager.java
@@ -0,0 +1,28 @@
+import java.util.Map;
+
+public class ConfigurationManager {
+    String hostname;
+    String port;
+    String nom;
+
+
+    private static ConfigurationManager instance;
+
+
+    private ConfigurationManager(String hostname, String port, String nom) {
+this.hostname = hostname;
+this.port = port;
+this.nom = nom;
+    }
+
+    public static synchronized ConfigurationManager getInstance(Map<String,String> map) {
+        String hostname = map.get("db.host");
+String port = map.get("db.port");
+String nom = map.get("app.name");
+
+            if (instance == null) {
+                instance = new ConfigurationManager(hostname, port, nom);
+            }
+            return instance;
+        }
+}
