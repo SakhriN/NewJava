f018a18645e62419f70f9c06b3ee6ecf2e35cf85
Nassim Sakhri <tails01@hotmail.fr>
2024-12-06 16:42:17 +0100

    modification

--- ExoCommand/src/Playlist.java
+++ ExoCommand/src/Playlist.java
@@ -15,19 +15,19 @@ public class Playlist implements Command {
     public void execute(String command, Music music) {
         if(command.equals("add")){
             if(!musicList.contains(music)){
-                System.out.println("La musique suivante : "+ music.getName() + " n'existe pas dans la playlist, ajout en cours... ");
+                System.out.println("La musique suivante : \""+ music.getName() + "\" n'existe pas dans la playlist, ajout en cours... ");
                 addMusic(music);
                 System.out.println("Ajout terminé.");
             }else {
-                System.out.println("La musique "+music.getName()+" existe deja dans la playlist.");
+                System.out.println("La musique \""+music.getName()+"\" existe deja dans la playlist.");
             }
         }else{
             if(command.equals("remove")){
                 if(musicList.contains(music)){
-                    System.out.println("La musique suivante : "+ music.getName() + " existe dans la playlist, suppression en cours... ");
+                    System.out.println("La musique suivante : \""+ music.getName() + "\" existe dans la playlist, suppression en cours... ");
                     removeMusic(music);
                 }else{
-                    System.out.println("La musique "+music.getName()+" n'existe pas dans la playlist.");
+                    System.out.println("La musique \""+music.getName()+"\" n'existe pas dans la playlist.");
                 }
             }
         }
